45db88cafe30c551e1b5b1b5f7af59699416e08f
RadomirPopovicFON <popovirr@gesis.org>
2018-07-26 13:02:41 +0200

    Added cumulative csv visualization over time from the start

--- DataRetrieval/csvManipulation.py
+++ DataRetrieval/csvManipulation.py
@@ -10,31 +10,40 @@ def giveFullPandaFiles(): #return list of tuples with format : (path, concatenat
     finalListOfDictionaries = []
     temporaryKey = ""
     listIterator = -1
+
+    cumulativeValue = 0
+
     for key, value in readJson.findJsonFiles().items():
         temporaryPath = readJson.getShortName2(key)[0] + "_" + readJson.getShortName2(key)[1]
-        if(isMonthlyJsonFile(key)):
+        if(isMonthlyJsonFile(key)==True):
 
             tuple = (temporaryPath, [value])
-
-            #print(temporaryKey, " VS ", tuple[0])
             #if its equal to the previous one
             if not((temporaryKey==tuple[0])):
                 temporaryListOfDictionaries.append(tuple)
                 listIterator = listIterator + 1
             else:
                 temporaryListOfDictionaries[listIterator][1].append(value)
-
             temporaryKey = tuple[0]
 
     for i,j in temporaryListOfDictionaries:
-        finalListOfDictionaries.append((i, pd.concat(j)))
+        finalListOfDictionaries.append((i, pd.concat(j).reset_index(drop=True)))
 
     del temporaryListOfDictionaries[:]
     return finalListOfDictionaries
 
+def concatDataFrameLists(list):
+    cumulativeValue = 0
+    for df in list:
+        cumulativeValue = df.iloc[[-1][0]]
+        print(df, "\n\n\n")
+
 def isMonthlyJsonFile(jsonFile):
     lastPartName = readJson.getShortName2(jsonFile)[2]
-    return (False, True)[lastPartName.count('-')==1]
+    if (lastPartName.count('-')==1) :
+        return True
+    else:
+        return False
 
 def writeToCSVFile():
     pandaFiles = giveFullPandaFiles() #list of tuples
@@ -43,9 +52,9 @@ def writeToCSVFile():
         nameOfTheFile = CSV_FOLDER + key + ".csv"
         df = value
         with open(nameOfTheFile, 'wb') as csvfile:
-            filewriter = csv.writer(csvfile, delimiter=',',
+            filewriter = csv.writer(csvfile, delimiter='\t',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        df.to_csv(nameOfTheFile, sep='\t', encoding='utf-8')
+        df.to_csv(nameOfTheFile, sep='\t', encoding='utf-8', index=False)
 
 writeToCSVFile()
 
--- Visualization/basicPlot.py
+++ Visualization/basicPlot.py
@@ -1,4 +1,5 @@
 from DataRetrieval import readJson
+from DataRetrieval import csvManipulation
 import matplotlib as mat
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
@@ -12,35 +13,36 @@ from datetime import datetime
 import sys
 from PIL import Image
 import math
+
 from enum import Enum
-#these are not constant values should be changed
-IMAGES_FOLDER = os.path.dirname(os.getcwd())+"\Visualization\Images\\"
-#REPOSITORY_NAME = readJson.receiveRepositoryName()+"\\"
+
+# these are not constant values should be changed
+IMAGES_FOLDER = os.path.dirname(os.getcwd()) + "\Visualization\Images\\"
 
 
+# REPOSITORY_NAME = readJson.receiveRepositoryName()+"\\"
 def timeStampPlotter():
-
     n = 20
     duration = 1000
     now = time.mktime(time.localtime())
     timestamps = np.linspace(now, now + duration, n)
     dates = [dt.datetime.fromtimestamp(ts) for ts in timestamps]
 
-def testPlotterCumulativeCount():
 
+def testPlotterCumulativeCount():
     for key, value in readJson.findJsonFiles().items():
         subcategory = readJson.getShortName2(path=key)[1]
         name = readJson.getShortName(path=key)
         df = value
 
-        if(df.values.size==0):
+        if (df.values.size == 0):
             print(name, " has empty values")
             continue
 
         timestampValues = df.values[:, 1]
 
-        if(subcategory=='views'):
-            if(isinstance(timestampValues[0], pd._libs.tslibs.timestamps.Timestamp)):
+        if (subcategory == 'views'):
+            if (isinstance(timestampValues[0], pd._libs.tslibs.timestamps.Timestamp)):
                 timestampValues = [str(timeStampValue.date()) for timeStampValue in timestampValues]
                 example = df.ix[:, 'count']
                 test = example.cumsum()
@@ -55,25 +57,25 @@ def testPlotterCumulativeCount():
                 plt.title(title)
                 savePlotAsAnImage(plt, name=name, type='cumulative')
 
-def testPlotterHistogram():
 
+def testPlotterHistogram():
     for key, value in readJson.findJsonFiles().items():
         subcategory = readJson.getShortName2(path=key)[1]
         name = readJson.getShortName(path=key)
         df = value
 
-        if(df.values.size==0):
+        if (df.values.size == 0):
             continue
 
         timestampValues = df.values[:, 1]
 
-        if(subcategory=='views'):
+        if (subcategory == 'views'):
             if (isinstance(timestampValues[0], pd._libs.tslibs.timestamps.Timestamp)):
                 timestampValues = [str(timeStampValue.date()) for timeStampValue in timestampValues]
                 df = df.drop('timestamp', 1)
                 fig, axes = plt.subplots(nrows=2, ncols=1)
                 for i, c in enumerate(df.columns):
-                    df[c].plot(kind='bar', ax=axes[i], figsize=(12, 10), title=c.upper()+" : "+name)
+                    df[c].plot(kind='bar', ax=axes[i], figsize=(12, 10), title=c.upper() + " : " + name)
 
                     if (i == len(df.columns) - 1):
                         axes[i].set_xticklabels(timestampValues)
@@ -81,13 +83,14 @@ def testPlotterHistogram():
                         axes[i].set_xticklabels([])
                 savePlotAsAnImage(plt, name=name, type='histogram')
 
+
 def testPlotterHistogram2():
     colors = ['red', 'tan', 'lime']
     n_bins = 3
     name = readJson.giveDataFrameExample()[0]
     df = readJson.giveDataFrameExample()[1]
 
-    timestampValues = df.values[:,1]
+    timestampValues = df.values[:, 1]
     timestampValues = [str(timeStampValue.date()) for timeStampValue in timestampValues]
 
     df = df.drop('timestamp', 1)
@@ -102,28 +105,28 @@ def testPlotterHistogram2():
 
     savePlotAsAnImage(plt, name=name, type='histogram')
 
-def savePlotAsAnImage(plt, name, type):
 
-    newpath = IMAGES_FOLDER+"\\"+type+"\\"
+def savePlotAsAnImage(plt, name, type):
+    newpath = IMAGES_FOLDER + "\\" + type + "\\"
     if not os.path.exists(newpath):
         os.makedirs(newpath)
-    fullPathAndName =  newpath+str(name)+"_"+type+".png"
+    fullPathAndName = newpath + str(name) + "_" + type + ".png"
     plt.savefig(fullPathAndName)
 
-def mergePngFiles(type):
 
+def mergePngFiles(type):
     images_list = specialList = readJson.findPngFiles(type)
     imgs = [Image.open(i) for i in images_list]
     dimension = int(math.sqrt(math.ceil(len(images_list))))
     listOfHorizontalImages = []
 
     for k in range(dimension):
-        min_img_shape = sorted([(np.sum(i.size), i.size) for i in imgs [k:dimension+k] ])[0][1]
-        img_merge = np.hstack((np.asarray(i.resize(min_img_shape, Image.ANTIALIAS)) for i in imgs [k:dimension+k] ))
+        min_img_shape = sorted([(np.sum(i.size), i.size) for i in imgs[k:dimension + k]])[0][1]
+        img_merge = np.hstack((np.asarray(i.resize(min_img_shape, Image.ANTIALIAS)) for i in imgs[k:dimension + k]))
 
         # save the horizontally merged images
         img_merge = Image.fromarray(img_merge)
-        path = str(k)+"temporary.png"
+        path = str(k) + "temporary.png"
         listOfHorizontalImages.append(path)
         img_merge.save(path)
 
@@ -131,19 +134,52 @@ def mergePngFiles(type):
     min_img_shape = sorted([(np.sum(i.size), i.size) for i in imgs[0:dimension]])[0][1]
     img_merge = np.vstack((np.asarray(i.resize(min_img_shape, Image.ANTIALIAS)) for i in imgs))
     img_merge = Image.fromarray(img_merge)
-    img_merge.save("Full_"+type+".png")
+    img_merge.save("Full_" + type + ".png")
 
     dirTest = os.path.dirname(os.getcwd()) + "\Visualization\\"
-    for file in os.listdir(os.path.dirname(dirTest) ):
+    for file in os.listdir(os.path.dirname(dirTest)):
         if file.endswith('temporary.png'):
             print(file)
             os.remove(file)
 
-#def runPlotForEveryRepository():
+
+def visualizeCSV():
+
+    pandaFiles = csvManipulation.giveFullPandaFiles()
+
+    for key, value in pandaFiles:
+
+        name = key
+        df = value
+
+        timestampValues = df.values[:, 1]
+        if (df.values.size == 0):
+            print(name, " has empty values")
+            continue
+
+        timestampValues = df.values[:, 1]
+
+        if (isinstance(timestampValues[0], pd._libs.tslibs.timestamps.Timestamp)):
+            timestampValues = [str(timeStampValue.date()) for timeStampValue in timestampValues]
+            example = df.ix[:, 'count']
+            test = example.cumsum()
+            plt.figure();
+            test.plot();
+            plt.legend(loc='best')
+
+            # fig, ax = plt.subplots()
+            # ax.tick_params(labelbottom=False)
+
+            title = "Info from " + timestampValues[0] + " to " + timestampValues[-1] + " \nfor " + " : " + name
+            plt.title(title)
+            savePlotAsAnImage(plt, name=name, type='cumulative')
 
 
-#testPlotterCumulativeCount()
-#testPlotterHistogram()
-mergePngFiles("cumulative")
+# def runPlotForEveryRepository():
 
 
+# testPlotterCumulativeCount()
+# testPlotterHistogram()
+# mergePngFiles("cumulative")
+
+visualizeCSV()
