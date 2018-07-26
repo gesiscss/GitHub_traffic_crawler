d6f7871025a52b387d8a43336f3c9b1b13895261
RadomirPopovicFON <popovirr@gesis.org>
2018-07-26 16:03:19 +0200

    Small changes

--- DataRetrieval/readRepositories.py
+++ DataRetrieval/readRepositories.py
@@ -19,16 +19,15 @@ def getShortNameRepository(fullPath):
 def getRequestContributors():
 
     repos = [repo['name'] for repo in requests.get("https://api.github.com/orgs/gesiscss/repos").json()]
-    path = "https://api.github.com/repos/gesiscss/" + repos[0] + "/stats/contributors"
+    path = "https://api.github.com/repos/gesiscss/" + "kubespawner" + "/stats/contributors"
     response = requests.get(path).json()
-    print(response,"\n\n\n\n")
+    #print(response,"\n\n\n\n")
     #API can sometimes reply with - rate limit exceeded for this server.. make an offline json file
     dataFrame = pd.DataFrame.from_dict(response)
     # listOfRepositories = dataFrame['archive_url'].values
     # listOfRepositories = [getShortNameRepository(repository) for repository in listOfRepositories]
     return dataFrame
 
-print(getRequestContributors())
 
 
 
